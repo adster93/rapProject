9d8159e06493117bf377fb662e90c233c0f62822
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2015-11-20 00:44:00 -0700

    some bug fixes and localstorage

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
-
-gulp.task('default', function(){
-	gulp
-		.src('*.js')
-		.pipe(gulp.dest('./test'))
+gulp.task('default', function() {
+    gulp.src('*.js').pipe(gulp.dest('./test'))
 })
-
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch(['*.js', '*.scss']);
+    livereload.listen();
+    gulp.watch(['*.js', '*.scss']);
 });
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(function() {
             success: function(data) {
                 console.log(data)
                     //console.log(data[0].words)
-                var rhymeWords = []; 
+                var rhymeWords = [];
                 if (data[0]) {
                     $('#errorMessage').text('')
                     for (i = 0; i < data[0].words.length; i++) {
@@ -21,14 +21,6 @@ $(function() {
                 } else {
                     $('#errorMessage').text('Choose another word! (p.s. no $ymb0l$!)')
                     localStorage.removeItem(JSON.stringify(words[i]))
-                //     for(var i = 0, len = localStorage.length; i < len; ++i){
-                //     if(localStorage.getItem(JSON.parse(localStorage.key(i))) !== undefined){
-                //         console.log('word is good')
-                //         localStorage.setItem(query, JSON.stringify(rhymeWords))
-                //     }
-                     
-                // }
-                     
                 }
                 $('#rhymeBox').empty()
                 for (i = 0; i < rhymeWords.length; i++) {
@@ -39,13 +31,11 @@ $(function() {
             error: function(ajaxContext) {
                 $('#errorMessage').text('Please enter a word!')
                 localStorage.removeItem(JSON.stringify(words[i]))
-
             }
         })
         return false
     }
     $('#subBut').on('click', wordInput)
-        // $('#subBut').on('click', addDiv)
 })
 $(function() {
     $('#searchBox').focus().css('outline-color', '#FF0000')
@@ -54,38 +44,18 @@ $(function() {
     $('#subBut').focus().css('outline-color', '#FF0000')
 })
 $(function() {
-            for (var i = 0, len = localStorage.length; i < len; ++i) {
-                console.log(localStorage.getItem(localStorage.key(i)));
-                var $div = $('<div class=columnDivs>' + '<p class=highlight>' + localStorage.key(i) + '</p>' + '</div>')
-                $("#recentWords").append($div)
-                //below works, above testing
-                //$("#recentWords").append('<div id=div'+ i + '>' + '<p>' + localStorage.key(i) + '</p>' + '</div>');
-                // for(var i=0; len = localStorage.getItem(localStorage.key(i).length), i < len; ++i){
-                    var recentWord = JSON.parse(localStorage.getItem((localStorage.key(i))))
-                    for(var j=0; len = recentWord.length, j < len; ++j){
-                        // $('#recentWords').append('<p>' + recentWord[i] + '</p>')
-                        $div.append('<p>' + recentWord[j] + '</p>')
-
-                    }
-                console.log(recentWord)
-
-            }
-        })
-
-//'<p>' + localStorage.getItem((localStorage.key(i))) + '</p>'
-
-
-    // <script type="text/javascript" src="scripts/soundcloud.player.api.js"></script>
-    //  </script>
-    // soundcloud.addEventListener('onPlayerReady', function(player, data) {
-    //            player.api_getTracks();
-    // addDiv = function(){
-    //              $('#subBut').on('click', function(){
-    //                  for(i=0; i<rhymeWords.length; i++){
-    //                  $('#rhymeBox').append( $("#rhymeBox").append() + rhymeWords[i] + '<br/>');
-    // $('#subBut').on('click', function(){
-    //  return 
-    // })
-    // $('').click(function(){
-    //  alert('handler is clicked')
-    // })
+    for (var i = 0, len = localStorage.length; i < len; ++i) {
+        console.log(localStorage.getItem(localStorage.key(i)));
+        var $div = $('<div class=columnDivs>' + '<p class=highlight>' + localStorage.key(i) + '</p>' + '</div>')
+        $("#recentWords").append($div)
+            //below works, above testing
+            //$("#recentWords").append('<div id=div'+ i + '>' + '<p>' + localStorage.key(i) + '</p>' + '</div>');
+            // for(var i=0; len = localStorage.getItem(localStorage.key(i).length), i < len; ++i){
+        var recentWord = JSON.parse(localStorage.getItem((localStorage.key(i))))
+        for (var j = 0; len = recentWord.length, j < len; ++j) {
+            // $('#recentWords').append('<p>' + recentWord[i] + '</p>')
+            $div.append('<p>' + recentWord[j] + '</p>')
+        }
+        console.log(recentWord)
+    }
+})
