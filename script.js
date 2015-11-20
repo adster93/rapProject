d259c6366c89b24f36f529bc8a2eb96b85d6cf43
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2015-11-19 22:59:26 -0700

    changed script.js to gh-pages

--- script.js
+++ script.js
@@ -1,67 +1,94 @@
-$(function(){
-	wordInput = function(){
-		var query = $('#searchBox').val();
-		var number = $('#num').val()
-		console.log(query)
-		console.log(number)
-		$.ajax({
-			type: 'GET',
-			dataType: 'json',
-			url: 'http://api.wordnik.com:80/v4/word.json/' + query + '/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=' + number + '&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-			success: function(data){
-				console.log(data)
-				//console.log(data[0].words)
-				var rhymeWords = [];
-				if(data[0]){
-					for(i=0; i<data[0].words.length; i++){
-					rhymeWords.push(data[0].words[i])
-					//console.log(data[0].words[i])
-				}
-					}
-					else{
-        		$('#explain').append('<li id="error">' + 'Choose another word! (p.s. no $ymb0l$!)' + '</li>') 
-        		$('#error').fadeOut(8000, function(){
-        			$(this).remove(); 
-        		});
-					}
-				$('#rhymeBox').empty()
-				for(i=0; i<rhymeWords.length; i++){
-					$('#rhymeBox').append('<p>' + rhymeWords[i] + '<p>')
-				};
-				console.log(rhymeWords)
-			 },
-			 error: function (ajaxContext) {
-        		$('#explain').append('<li id="error">' + 'Please enter a word!' + '</li>') 
-        		$('#error').fadeOut(5000, function(){
-        			$(this).remove(); 
-        		});
-        	}
-			})
-		return false
-		}
-		$('#subBut').on('click', wordInput)
-		// $('#subBut').on('click', addDiv)
-	})
-
-$(function(){
+$(function() {
+    wordInput = function() {
+        var query = $('#searchBox').val().toLowerCase();
+        var number = $('#num').val()
+        console.log(query)
+        console.log(number)
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: 'http://api.wordnik.com:80/v4/word.json/' + query + '/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=' + number + '&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
+            success: function(data) {
+                console.log(data)
+                    //console.log(data[0].words)
+                var rhymeWords = [];
+                if (data[0]) {
+                    $('#errorMessage').text('')
+                    for (i = 0; i < data[0].words.length; i++) {
+                        rhymeWords.push(data[0].words[i])
+                            //console.log(data[0].words[i])
+                    }
+                } else {
+                    $('#errorMessage').text('Choose another word! (p.s. no $ymb0l$!)')
+                        // $('#explain').append('<li id="error">' + 'Choose another word! (p.s. no $ymb0l$!)' + '</li>') 
+                        // $('#errorMessage').fadeOut(3000, function(){
+                        // });
+                }
+                $('#rhymeBox').empty()
+                for (i = 0; i < rhymeWords.length; i++) {
+                    $('#rhymeBox').append('<p>' + rhymeWords[i] + '<p>')
+                        // $(this).remove(); 
+                };
+                localStorage.setItem(query, JSON.stringify(rhymeWords))
+                    // if (localStorage.getItem('word') !== null) {
+                    //     for (i = 0; i < localStorage.length; i++) {
+                    //         localStorage.setItem('word' + i, JSON.stringify(rhymeWords))
+                    //     }
+                    // } else {
+                    //     localStorage.setItem('word', JSON.stringify(rhymeWords))
+                    // }
+            },
+            error: function(ajaxContext) {
+                $('#errorMessage').text('Please enter a word!')
+                    // $('#explain').append('<li id="error">' + 'Please enter a word!' + '</li>') 
+                    // $('#errorMessage').fadeOut(3000, function(){
+                    // $(this).remove(); 
+                    // });`
+            }
+        })
+        return false
+    }
+    $('#subBut').on('click', wordInput)
+        // $('#subBut').on('click', addDiv)
+})
+$(function() {
     $('#searchBox').focus().css('outline-color', '#FF0000')
-    })
-
-$(function(){
+})
+$(function() {
     $('#subBut').focus().css('outline-color', '#FF0000')
-    })
-	
+})
+$(function() {
+            for (var i = 0, len = localStorage.length; i < len; ++i) {
+                console.log(localStorage.getItem(localStorage.key(i)));
+                var $div = $('<div class=columnDivs>' + '<p class=highlight>' + localStorage.key(i) + '</p>' + '</div>')
+                $("#recentWords").append($div)
+                //below works, above testing
+                //$("#recentWords").append('<div id=div'+ i + '>' + '<p>' + localStorage.key(i) + '</p>' + '</div>');
+                // for(var i=0; len = localStorage.getItem(localStorage.key(i).length), i < len; ++i){
+                    var recentWord = JSON.parse(localStorage.getItem((localStorage.key(i))))
+                    for(var j=0; len = recentWord.length, j < len; ++j){
+                        // $('#recentWords').append('<p>' + recentWord[i] + '</p>')
+                        $div.append('<p>' + recentWord[j] + '</p>')
 
-// addDiv = function(){
-// 				$('#subBut').on('click', function(){
-// 					for(i=0; i<rhymeWords.length; i++){
-// 					$('#rhymeBox').append( $("#rhymeBox").append() + rhymeWords[i] + '<br/>');
-// $('#subBut').on('click', function(){
-// 	return 
+                    }
+                console.log(recentWord)
 
-// })
+            }
+        })
+//'<p>' + localStorage.getItem((localStorage.key(i))) + '</p>'
 
 
-// $('').click(function(){
-// 	alert('handler is clicked')
-// })
+    // <script type="text/javascript" src="scripts/soundcloud.player.api.js"></script>
+    //  </script>
+    // soundcloud.addEventListener('onPlayerReady', function(player, data) {
+    //            player.api_getTracks();
+    // addDiv = function(){
+    //              $('#subBut').on('click', function(){
+    //                  for(i=0; i<rhymeWords.length; i++){
+    //                  $('#rhymeBox').append( $("#rhymeBox").append() + rhymeWords[i] + '<br/>');
+    // $('#subBut').on('click', function(){
+    //  return 
+    // })
+    // $('').click(function(){
+    //  alert('handler is clicked')
+    // })
